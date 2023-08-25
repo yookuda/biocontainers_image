c8be67404cface163faeb9c2d9b449e67597f331
Yoshihiro Okuda <yookuda@nig.ac.jp>
2023-08-25 10:47:31 +0900

    add get_command_and_shared_library_list_qsub.sh

---
+++ get_command_and_shared_library_list_qsub.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# 引数でイメージのリストを指定し、リスト中に存在しないbiotoolsイメージからイメージ内の/usr以下の共有ライブラリ・/usr/local/binのコマンドを取得する。
+
+function output_and_exec_script () {
+    echo '#!/bin/bash' > $1
+    echo '#$ -S /bin/bash' >> $1
+    echo '#$ -V' >> $1
+    echo '#$ -cwd' >> $1
+    echo "DIR=\"${2}\"" >> $1
+    echo 'echo "DIR:${DIR}"' >> $1
+    echo "BASE_LIST=\"${3}\"" >> $1
+    echo 'for IMAGE in $(ls ${DIR}); do' >> $1
+    echo 'if grep -q ${IMAGE} ${BASE_LIST} ; then' >> $1
+    echo ':' >> $1
+    echo 'else' >> $1
+    echo 'echo "IMAGE:${IMAGE}"' >> $1
+    echo 'singularity exec ${DIR}/${IMAGE} find /usr -name "*.so" -or -name "*.so.*" | xargs -I {} echo "LIBRARY:"{}' >> $1
+    echo 'singularity exec ${DIR}/${IMAGE} ls /usr/local/bin |  xargs -I {} echo "COMMAND:"{}' >> $1
+    echo 'fi' >> $1
+    echo 'done' >> $1
+    qsub $1
+}
+
+BASE_LIST=$1
+
+for D in $(ls /usr/local/biotools); do
+    if [ ${D} = 'bioconductor' ]; then
+        for D2 in $(ls /usr/local/biotools/bioconductor); do
+            SCRIPT="biotools_${D}_${D2}.sh"
+            DIR="/usr/local/biotools/${D}/${D2}"
+            output_and_exec_script ${SCRIPT} ${DIR} ${BASE_LIST}
+        done
+    else
+        SCRIPT="biotools_${D}.sh"
+        DIR="/usr/local/biotools/${D}"
+        output_and_exec_script ${SCRIPT} ${DIR} ${BASE_LIST}
+    fi
+done
+
